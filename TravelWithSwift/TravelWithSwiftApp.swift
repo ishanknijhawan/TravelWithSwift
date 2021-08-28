ed7076d68de9aeac5425983769e5e1b2dcadbb00
ishanknijhawan <ishanknijhawan@gmail.com>
2021-08-29 01:12:23 +0530

    disvover screen

--- TravelWithSwift/TravelWithSwiftApp.swift
+++ TravelWithSwift/TravelWithSwiftApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct TravelWithSwiftApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
