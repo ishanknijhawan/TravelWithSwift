89dd860b2215f492a389c23005320234245ffb3f
ishanknijhawan <ishanknijhawan@gmail.com>
2021-08-28 21:47:43 +0530

    Initial Commit

---
+++ TravelWithSwift/TravelWithSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelWithSwiftApp.swift
+//  TravelWithSwift
+//
+//  Created by Ishank Nijhawan on 28/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TravelWithSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
